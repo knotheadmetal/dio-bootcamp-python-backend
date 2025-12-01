2e3a98474b52434ddda4fe4b9fe445ba7ee1d9db
knotheadmetal <rafaelsantosti@outlook.com>
2025-12-01 00:28:34 -0300

    aula4strings

---
+++ 4-Dominando Strings e Fatiamento/string_3.py
@@ -0,0 +1,10 @@
+nome = "Rafael da Silva Santos"
+
+print(nome[0])      # Primeiro caractere
+print(nome[-2])    # Penúltimo caractere
+print(nome[:9])   # Do início até o índice 8
+print(nome[10:])  # Do índice 10 até o final
+print(nome[10:16])  # Do índice 10 até o 15
+print(nome[10:16:2])  # Do índice 10 até o 15, pulando de 2 em 2
+print(nome[:])  # A string completa
+print(nome[::-1])  # A string de trás para frente
---
+++ 4-Dominando Strings e Fatiamento/string_4.py
@@ -0,0 +1,9 @@
+nome = "Rafael"
+
+mensagem = f"""
+Olá meu nome é {nome},
+seja bem-vindo ao curso de Python!
+Espero que você aproveite muito o conteúdo.
+"""
+
+print(mensagem) 
