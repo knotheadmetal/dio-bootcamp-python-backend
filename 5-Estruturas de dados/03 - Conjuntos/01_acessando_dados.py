3908e40976d246d3bc3f683ad37ca7017258f7ef
knotheadmetal <rafaelsantosti@outlook.com>
2026-01-05 22:57:55 -0300

    update conjuntos 01 - acessando dados

--- 5-Estruturas de dados/03 - Conjuntos/01_acessando_dados.py
+++ 5-Estruturas de dados/03 - Conjuntos/01_acessando_dados.py
@@ -1,5 +1,5 @@
-numeros = {1, 2, 3, 2}
+numeros = {1, 2, 3, 2} # Conjunto de números
 
-numeros = list(numeros)
+numeros = list(numeros) # Convertendo para lista
 
-print(numeros[0])
+print(numeros[0]) # Acessando o primeiro elemento
