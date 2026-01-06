154185ed936e96e8a5cb843c47272f3575e2b5ba
knotheadmetal <rafaelsantosti@outlook.com>
2026-01-05 22:55:28 -0300

    update conjuntos 00

--- 5-Estruturas de dados/03 - Conjuntos/00_declarando_conjuntos.py
+++ 5-Estruturas de dados/03 - Conjuntos/00_declarando_conjuntos.py
@@ -1,8 +1,8 @@
-numeros = set([1, 2, 3, 1, 3, 4])
+numeros = set([1, 2, 3, 1, 3, 4]) # Conjunto de números
 print(numeros)  # {1, 2, 3, 4}
 
-letras = set("abacaxi")
+letras = set("abacaxi") # Conjunto de letras
 print(letras)  # {"b", "a", "c", "x", "i"}
 
-carros = set(("palio", "gol", "celta", "palio"))
+carros = set(("palio", "gol", "celta", "palio")) # Conjunto de carros
 print(carros)  # {"gol", "celta", "palio"}
